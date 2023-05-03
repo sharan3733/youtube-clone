286d52856b04ca151c743c938f200362b91a3e4b
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-05-03 17:44:59 +0200

    setup in datamodel

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Home Page</h1>
-      </div>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <h1>Home page</h1>
+      <a href='/api/auth/signin'>login</a>
     </main>
   )
 }

0c7718da8ec6f9e39579251bb66c73089f0cdbab
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-05-04 11:44:06 +0200

    links to the single video page

--- pages/components/Video.js
+++ pages/components/Video.js
@@ -1,17 +1,27 @@
 import Image from "next/image"
+import timeago from "@/lib/timeago"
+import Link from "next/link"
 
 export default function Video({video}) {
     return (
         <div className=''>
             <div className='px-5 pb-5'>
                {video.thumbnail &&(
+                <Link href={`/video/${video.id}`}>
                 <Image
                 className='mb-2 cursor-pointer'
                 src={video.thumbnail}
                 width='800'
                 height='450'
                 />
+                </Link>
                )}
+               <p className='text-white float-right relative -mt-11 mr-1 bg-black p-1'>
+                {Math.floor(video.length/60)
+                .toString()
+                .padStart(2, '0')}
+                :{(video.length % 60).toString().padStart(2, '0')}
+               </p>
                
                <div className='flex'>
                 {video.author.image && (
@@ -21,11 +31,17 @@ export default function Video({video}) {
                     />
                 )}
                 <div>
-                    <p className="text-lg font-bold text-white">{video.title}</p>
+                    <Link href={`/video/${video.id}`}
+                     className="text-lg font-bold text-white">{video.title}
+                     </Link>
                     <div className="">
                         <div className="">
                             <div className="text-gray-400">
-                                <span className="mr-2 underline">{video.author.name}</span>
+                                <Link href={`/channel/${video.author.username}`}
+                                className="mr-2 underline cursor-pointer">
+                                    {video.author.name}</Link>
+                                . {video.views} views .{' '}
+                                {timeago.format(new Date(video.createdAt))}
                             </div>
                         </div>
                     </div>
@@ -34,7 +50,7 @@ export default function Video({video}) {
             </div>
                 
         
-        <p className="text-lg font-bold text-white">{video.title}</p>
-        </div>
+    
+        </div> 
     )
 }

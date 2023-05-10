20126536f271304a588402720e10f119c6f95b63
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-05-10 07:57:21 +0200

    updates

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -7,10 +7,10 @@ middleware.use(async (req, res, next) => {
   const form = new multiparty.Form()
 
   await form.parse(req, function (err, fields, files) {
-        req.body = fields
-        req.files = files
-        next()
-    })
+    req.body = fields
+    req.files = files
+    next()
+  })
 })
 
 export default middleware
--- pages/components/Heading.js
+++ pages/components/Heading.js
@@ -5,17 +5,17 @@ import { useSession } from "next-auth/react";
 export default function Heading() {
     const router = useRouter()
 
-    const {data: session, status} = useSession()
+    const { data: session, status } = useSession()
     const loading = status === 'loading'
 
-    if(loading) {
+    if (loading) {
         return null
     }
 
     return (
         <header className="h-14 flex pt-5 px-5 pb-2">
             <div className="text-xl">
-                {router.asPath ==='/' ? (
+                {router.asPath === '/' ? (
                     <p>YouTube clone</p>
                 ) : (
                     <Link href={`/`} className='underline'>Home</Link>
@@ -23,11 +23,21 @@ export default function Heading() {
             </div>
             <div className="grow ml-10 -mt-1"></div>
 
+            {session && (
+                <Link href={`/channel/${session.user.username}`} className="flex">
+                    <img
+                        src={session.user.image}
+                        className="h-8 mr-2 mb-2 -mt-1 w-8 rounded-full"
+                    />
+                    <p className="mr-3">{session.user.name}</p>
+                </Link>
+            )}
+
             <a
-            className="flex-l border px-4 font-bold rounded-full"
-            href={session ? '/api/auth/signout' : '/api/auth/signin'}
+                className="flex-l border px-4 font-bold rounded-full"
+                href={session ? '/api/auth/signout' : '/api/auth/signin'}
             >
-                {session ? 'logout':'login'}
+                {session ? 'logout' : 'login'}
             </a>
         </header>
     )

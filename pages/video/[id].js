9baf09a4943bcd4fd97180b8f1b19deb5a1f1482
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-05-11 11:04:18 +0200

    views count increment by 2

--- pages/video/[id].js
+++ pages/video/[id].js
@@ -6,63 +6,81 @@ import Video from "../components/Video";
 
 import dynamic from "next/dynamic";
 import Heading from "../components/Heading";
-
+import { useEffect } from "react";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export default function SingleVideo({ video, videos }) {
-  if (!video) return <p className="text-center p-5">Video does not exist</p>;
+  if (!video) return <p className="text-center p-5">Video does not exist</p>
+
+
+  useEffect(() => {
+    const incrementViews = async () => {
+      await fetch('/api/view', {
+        body: JSON.stringify({
+          video: video.id,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      })
+    }
+    incrementViews()
+  }, []);
+
   return (
     <>
-      <Heading/>
+      <Heading />
 
-        <div className="h-screen flex">
-          <div className="flex w-full md:w-2/3 flex-col mb-4 border-t border-r border-b border-3 border-black pl-0 bg-black">
-            <div className="relative pt-[60%]">
-              <ReactPlayer
-                className="react-player absolute top-0 left-0"
-                url={video.url}
-                width="100%"
-                height="100%"
-                controls={true}
-                light={video.thumbnail}
-              />
-            </div>
-
-            <div className="px-5 mt-5">
-                <div className="flex">
-                    <div>
-                        <p className="text-2xl font-bold">{video.title}</p>
-                        <div className="text-gray-400">
-                            {video.views} views .{' '}
-                            {timeago.format(new Date(video.createdAt))}
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-between border-t border-gray-500 mt-5 pt-5'>
-                    <Link href={`/channel/${video.author.username}`} className='flex'>
-                        {video.author.image &&(
-                            <img
-                               className="w-16 h-16 mt-2 mr-2 rounded-full"
-                               src={video.author.image}
-                               />
-                        )}
-                    <span className="mt-6 ml-2 text-xl">{video.author.name}</span>
-                    </Link>
-                </div>
-            </div>
+      <div className="h-screen flex">
+        <div className="flex w-full md:w-2/3 flex-col mb-4 border-t border-r border-b border-3 border-black pl-0 bg-black">
+          <div className="relative pt-[60%]">
+            <ReactPlayer
+              className="react-player absolute top-0 left-0"
+              url={video.url}
+              width="100%"
+              height="100%"
+              controls={true}
+              light={video.thumbnail}
+            />
           </div>
-          <div className="hidden md:block md:w-1/3">
-            <div className="flex flex-wrap">
-                {videos.map((video, index) => (
-                    <div className="w-full" key={index}>
-                        <Video video={video}/>
-                    </div>
-                ))}
+
+          <div className="px-5 mt-5">
+            <div className="flex">
+              <div>
+                <p className="text-2xl font-bold">{video.title}</p>
+                <div className="text-gray-400">
+                  {video.views} views .{' '}
+
+                  {timeago.format(new Date(video.createdAt))}
+                </div>
+              </div>
+            </div>
+            <div className='flex justify-between border-t border-gray-500 mt-5 pt-5'>
+              <Link href={`/channel/${video.author.username}`} className='flex'>
+                {video.author.image && (
+                  <img
+                    className="w-16 h-16 mt-2 mr-2 rounded-full"
+                    src={video.author.image}
+                  />
+                )}
+                <span className="mt-6 ml-2 text-xl">{video.author.name}</span>
+              </Link>
             </div>
           </div>
         </div>
-     
-      
+        <div className="hidden md:block md:w-1/3">
+          <div className="flex flex-wrap">
+            {videos.map((video, index) => (
+              <div className="w-full" key={index}>
+                <Video video={video} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+
     </>
   )
 }
